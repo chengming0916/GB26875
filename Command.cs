803878719d64c4e908c0ce478bcb848345d19a87
Chemmy <chengming0916@outlook.com>
2022-12-02 16:07:12 +0800

    添加Command枚举定义

---
+++ Command.cs
@@ -0,0 +1,16 @@
+﻿namespace GB26875
+{
+    /// <summary>
+    /// 控制单元命令字节定义
+    /// 0: 预留 7~127 预留 128~255 用户自行定义
+    /// </summary>
+    public enum Command : byte
+    {
+        Control = 1,
+        Report = 2,
+        Confirm = 3,
+        Request = 4,
+        Response = 5,
+        Deny = 6
+    }
+}
